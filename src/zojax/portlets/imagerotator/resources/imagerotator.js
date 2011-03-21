7b1a4277bfd3b4908c2e1c39328e783900f7205d
Dmitry Suvorov <suvdim@gmail.com>
2011-03-21 20:41:55 +0000

    added image preloader

--- src/zojax/portlets/imagerotator/resources/imagerotator.js
+++ src/zojax/portlets/imagerotator/resources/imagerotator.js
@@ -1,5 +1,50 @@
 /* zojax image rotator portlet */
 $(document).ready(function() {
+
+$.preLoadImages = function(imageList,callback) {
+  var pic = [], i, total, loaded = 0;
+  if (typeof imageList != 'undefined') {
+    if ($.isArray(imageList)) {
+      total = imageList.length; // used later
+      for (i=0; i < total; i++) {
+        pic[i] = new Image();
+        pic[i].onload = function() {
+          loaded++; // should never hit a race condition due to JS's non-threaded nature
+          if (loaded == total) {
+            if ($.isFunction(callback)) {
+              callback();
+            }
+          }
+        };
+        pic[i].src = imageList[i];
+      }
+    }
+    else {
+      pic[0] = new Image();
+      pic[0].onload = function() {
+        if ($.isFunction(callback)) {
+          callback();
+        }
+      }
+      pic[0].src = imageList;
+    }
+  }
+  pic = undefined;
+};
+
+$(".banners").find(".banner-item.first").before("<li class='preloader'> </li>");
+
+var toPreload=[];
+$(".banners").find("img").each(function(){
+  toPreload.push($(this).attr("src"));
+});
+
+$.preLoadImages(toPreload,function(){
+    $('.preloader').remove();
+  }
+);
+
+
 $(".buttons-block").hide();
 
 $('.imagerotator-container').each(function() {
